6f0d01d575ff7ebb333e314953d2fe59fca351c3
Vlad <vlivonika@gmail.com>
2024-07-26 13:11:16 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <CalculatorView />
+</template>
+
+<script>
+import CalculatorView from "./components/CalculatorView.vue";
+
+export default {
+  name: "App",
+  components: {
+    CalculatorView,
+  },
+};
+</script>
+
+<style>
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+#app {
+  font-family: Roboto, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  background-image: url(./assets/images/bubor5.jpg);
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  width: 100%;
+  height: 100vh;
+}
+</style>
---
+++ src/components/CalculatorView.vue
@@ -0,0 +1,205 @@
+<template>
+  <div class="main">
+    <div class="calculator">
+      <div class="display-text">{{ expression || "" }}</div>
+      <div class="display-result">{{ currentValue || "" }}</div>
+      <div
+        v-for="(button, key) in buttons"
+        :key="key"
+        class="btn"
+        :class="button.class ? button.class : ''"
+        @click="button.click"
+      >
+        {{ button.label }}
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      currentValue: "",
+      previousValue: "",
+      expression: "",
+      lastValue: "",
+      isActionEqual: false,
+    };
+  },
+  methods: {
+    clear() {
+      this.currentValue = "";
+      this.previousValue = "";
+      this.expression = "";
+      this.lastValue = "";
+      this.isActionEqual = false;
+    },
+    cancel() {
+      if (this.currentValue) {
+        this.currentValue = this.currentValue.slice(0, -1);
+        this.expression = this.expression.slice(0, -1);
+        this.isActionEqual = false;
+      }
+    },
+    percentage() {
+      this.currentValue = parseFloat(this.currentValue / 100).toString();
+    },
+    dot() {
+      if (this.currentValue.indexOf(".") === -1) this.append(".");
+    },
+    append(value) {
+      let newValue = value;
+
+      if (this.isActionEqual) {
+        if (!["+", "-", "*", "/", "="].includes(value)) {
+          this.expression = "";
+          this.currentValue = "";
+        } else {
+          this.expression = this.currentValue;
+        }
+        this.isActionEqual = false;
+      }
+
+      if (["+", "-", "*", "/", "="].includes(value)) {
+        this.currentValue = "";
+        let lastChar = this.expression.charAt(this.expression.length - 1);
+        if (["+", "-", "*", "/", "="].includes(lastChar))
+          this.expression = this.expression.slice(0, -1);
+      } else this.currentValue = `${this.currentValue}${value}`;
+
+      this.previousValue = value;
+      this.expression = `${this.expression}${newValue}`;
+    },
+    equal() {
+      if (!this.expression || this.isActionEqual) return false;
+      if (
+        !["+", "-", "*", "/"].some((operator) =>
+          this.expression.includes(operator)
+        )
+      )
+        return false;
+      let newExpression = this.expression;
+
+      let lastChar = this.expression.charAt(this.expression.length - 1);
+      if (["+", "-", "*", "/", "="].includes(lastChar)) {
+        if (lastChar == "=") {
+          newExpression = this.expression.slice(0, -1);
+        } else {
+          newExpression = `${this.expression}0`;
+          this.expression = newExpression;
+        }
+      }
+      this.append("=");
+      this.isActionEqual = true;
+
+      let ans = eval(newExpression);
+      if (ans == "Infinity") {
+        ans = "";
+        this.expression = "";
+      }
+      this.currentValue = ans.toString();
+    },
+  },
+  computed: {
+    buttons() {
+      return [
+        { label: "CE", click: this.cancel },
+        { label: "C", click: this.clear },
+        { label: "%", click: this.percentage },
+
+        { label: "1", click: this.append.bind(this, "1") },
+        { label: "2", click: this.append.bind(this, "2") },
+        { label: "3", click: this.append.bind(this, "3") },
+
+        { label: "4", click: this.append.bind(this, "4") },
+        { label: "5", click: this.append.bind(this, "5") },
+        { label: "6", click: this.append.bind(this, "6") },
+
+        { label: "7", click: this.append.bind(this, "7") },
+        { label: "8", click: this.append.bind(this, "8") },
+        { label: "9", click: this.append.bind(this, "9") },
+
+        { label: "0", click: this.append.bind(this, "0"), class: "zero" },
+        { label: ".", click: this.dot },
+        { label: "/", click: this.append.bind(this, "/"), class: "operator" },
+
+        { label: "*", click: this.append.bind(this, "*"), class: "operator" },
+        { label: "+", click: this.append.bind(this, "+"), class: "operator" },
+        { label: "-", click: this.append.bind(this, "-"), class: "operator" },
+
+        { label: "=", click: this.equal, class: "operator even" },
+      ];
+    },
+  },
+};
+</script>
+
+<style scoped>
+.main {
+  max-width: 1280px;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+}
+.calculator {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-auto-rows: minmax(53px, auto);
+  border: 2px solid #86bd3e;
+  margin-right: 40px;
+  margin-left: auto;
+  font-size: 22px;
+  font-weight: 500;
+  width: 340px;
+  background-color: #a5d67d;
+  border-radius: 20px;
+}
+.display-text {
+  grid-column: 1/4;
+  background-color: snow;
+  color: #000;
+  text-align: right;
+  font-size: 22px;
+  padding: 8px 45px 2px 0px;
+  border-radius: 20px 20px 0 0;
+}
+.display-result {
+  grid-column: 1/4;
+  background-color: #86bd3e;
+  color: #fff;
+  /* color: #000; */
+  text-align: right;
+  padding: 8px 45px 2px 0px;
+}
+.btn {
+  background-color: snow;
+  border: 1px solid #86bd3e;
+  padding: 2px;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 8px;
+}
+.operator {
+  background-color: snow;
+  color: #000;
+  width: 112px;
+}
+.even {
+  border-radius: 0 0 0px 0;
+  grid-column: 2/4;
+  border: 1px solid #86bd3e;
+}
+@media (max-width: 576px) {
+  .calculator {
+    margin-right: 20px;
+  }
+}
+@media (max-width: 414px) {
+  .calculator {
+    margin: 0 auto;
+  }
+}
+</style>
